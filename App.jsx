18dd42117976c7d3ecc79faf7839cfea59352543
Carlos <86490044+Cdireni@users.noreply.github.com>
2025-05-06 01:27:15 -0300

    Update App.jsx con agregado de import para presentacion saludo y productos
    Cuando agregué Hero saqué las otras lineas, aqui las agregué para que las pueda ver el Profe

--- App.jsx
+++ App.jsx
@@ -1,9 +1,18 @@
+import Saludo from './Saludo';
+import Presentacion from './Presentacion';
+import Productos from './Productos';
 import Hero from './componentes/Hero';
 
 
 function App() {
   return (
-    <div>
+     <div className="p-4">
+      <Saludo nombre="Carlos" />
+      <Presentacion nombre= "Carlos" edad={44} profesion="aprendiendo a desarrollar" barrio="Colinas Velez Sarsfield" />	
+      <button className="bg-blue-500 text-white rounded px-4 py-2 hover:bg-blue-700 mt-4">
+        Hacé clic aquí
+        </button>
+      <Productos />      
       <Hero />
     </div>
   );
